8b0bd00efb530ff5e3b72c2975f55e42754200ed
Johnathan Mitri <600018217@fjuhsd.org>
2021-02-16 00:55:43 +0000

    indented

--- Number.java
+++ Number.java
@@ -12,55 +12,54 @@
 public class Number
 {
 
-//private instance variable (must be an Integer)
-private int value;
-//constructor
-public Number()
-{
-  value = 0;
-}
-
-public Number(int a)
-{
-  value = a;
-}
-
-//getter method
-public int getValue()
-{
-  return value;
-}
-
-//setter method
-public void setValue(int newValue)
-{
-  value = newValue;
-}
-
-//isOdd method
-public boolean isOdd()
-{
-  return (value%2) == 1;
-}
-
-//isPerfect method
-public boolean isPerfect()
-{
-  int factorSum = 0;
-  for (int i = 1; i < value; i++)
+  //private instance variable (must be an Integer)
+  private int value;
+  //constructor
+  public Number()
   {
-    if (value % i == 0)
-    {
-      factorSum += i;
-    }
+    value = 0;
   }
-  return value == factorSum;
-}
 
-//toString
-public String toString()
-{
-  return "" + value;
-}
+  public Number(int a)
+  {
+    value = a;
+  }
 
+  //getter method
+  public int getValue()
+  {
+    return value;
+  }
+
+  //setter method
+  public void setValue(int newValue)
+  {
+    value = newValue;
+  }
+
+  //isOdd method
+  public boolean isOdd()
+  {
+    return (value%2) == 1;
+  }
+
+  //isPerfect method
+  public boolean isPerfect()
+  {
+    int factorSum = 0;
+    for (int i = 1; i < value; i++)
+    {
+      if (value % i == 0)
+      {
+        factorSum += i;
+      }
+    }
+    return value == factorSum;
+  }
+
+  //toString
+  public String toString()
+  {
+    return "" + value;
+  }
 }
